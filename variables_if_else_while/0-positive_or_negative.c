8bdd21fad9d6431966c54d073bbc521b5e1244d0
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-27 03:38:01 +0000

    +or-1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+ * main - Assigns a random number to variable (n) each time it is executed.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/* your code goes there */
+				if (n < 0)
+				{
+					printf("d% is negative\n", n);
+				}
+				else if (n == 0)
+				{
+					printf("d% is zero\n", n);
+				}
+				else
+				{
+					printf("d% is positive\n", n);
+				}
+					return (0);
+}
