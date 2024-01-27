e9bb237d036766931395c836361a25ceefa67307
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-27 08:17:48 +0000

    combo8

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,26 +11,14 @@ int main(void)
 	int d;
 
 	for (d = 48; d < 58; d++)
-	{
-		putchar(d);
 		{
-			if (d < 57)
+		putchar(d);
+		if (d != 57)
 			{
 			putchar(',');
-			{
-			if (d < 57)
-			{
 			putchar(' ');
-			{
-			if (d == 57)
-			{
-			putchar('\n');
-			}
-			}
-			}
-			}
 			}
 		}
-	}
+		putchar('\n');
 	return (0);
 }
