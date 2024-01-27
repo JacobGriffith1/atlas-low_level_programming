4e7f56605bb68d156080e47c58a2c3281df917bc
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-27 07:47:12 +0000

    combo

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <std.io>
+
+/**
+ * main - prints all single-digit numbers with commas and spaces
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 48; d < 58; d++)
+	{
+		putchar(d)
+		{
+			putchar(',')
+			{
+				putchar(' ')
+			}
+		}
+	}
+	return (0)
+}
