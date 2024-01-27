354dc727411fca7fd83f8c579fbc9e3d49b8a30c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-27 06:20:34 +0000

    noeq1

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+			if (ch == d)
+			{
+				(ch += 2);
+			}
+			if (ch == p)
+			{
+				(ch += 2);
+			}
+				{
+					putchar('\n');
+				}
+		}
+		return (0)
+}
