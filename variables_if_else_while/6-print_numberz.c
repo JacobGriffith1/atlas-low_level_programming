0774d220bf787bc1324805e0455590f99dc0d34c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-27 06:53:05 +0000

    numz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers zero to nine
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+		for (d = 0; d < 10; d++)
+		{
+			putchar(d);
+		}
+			{
+				putchar('\n')
+			}
+		return (0);
+}
