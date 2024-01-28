67600bd34fd73adf5b5f5a527034ec61ccc8b968
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 03:35:00 +0000

    times1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Prints times table for 0-9
+ *
+ * Return: N/A
+ */
+
+void times_table(void)
+{
+	int a, b, ab;
+
+	for (a = 0, a <= 9, a++)
+	{
+		_putchar(48);
+		for (b = 0, b <= 9, b++)
+		{
+			ab = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (ab <= 9)
+			{
+				_putchar(32);
+				_putchar(ab);
+			}
+			else
+			{
+				_putchar(ab / 10);
+				_putchar(ab % 10);
+			}
+		}
+	}
+	_putchar(10)
+}
