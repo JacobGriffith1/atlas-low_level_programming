6d9e803836628a1ef2eea7acef210a0cf1f0b094
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 07:28:57 +0000

    line1

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,2 +1,19 @@
 #include "main.h"
 
+/**
+ * print_line - Draws a straight line made of underscores.
+ *
+ * @n: Input for number of underscores.
+ *
+ * Return: N/A
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0, i < n; i++)
+	{
+		_putchar(95);
+	}
+}
