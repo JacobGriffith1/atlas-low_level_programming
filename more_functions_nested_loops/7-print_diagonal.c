92f94efba4e5f4ba14de9201a737639c93d7fb37
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 22:55:40 +0000

    diag1

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,2 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * print_diagonal - Prints a diagonal line descending from the left side.
+ *
+ *@n: Number of backslashes to print.
+ *
+ * Return: N/A
+ */
+
+void print_diagonal(int n)
+{
+	int p;
+	/**
+	 * @p: Number of backslashes actually printed.
+	 */
+
+	p = 0;
+
+	while (p < n)
+	{
+		_putchar(92);
+		_putchar(10);
+		_putchar(32);
+		p++;
+	}
+	_putchar(10);
+}
