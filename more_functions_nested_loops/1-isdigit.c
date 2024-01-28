539335547102973f17da0e3bf44505b3589d94b5
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 06:02:38 +0000

    dig1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,2 +1,21 @@
 #include "main.h"
 
+/**
+ * _isdigit - Checks for a digit.
+ *
+ * @c: Input character.
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
