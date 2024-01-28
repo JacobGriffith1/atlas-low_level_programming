64714bdef2bf6fbaec23fd5ab3f6178b3c6551e2
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 06:21:42 +0000

    print3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,16 @@
 /**
  * print_numbers - Prints numbers 0-9 and a newline.
  *
- * Return: Always 0 (Success)
+ * Return: N/A
  */
 
 void print_numbers(void)
 {
+	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
