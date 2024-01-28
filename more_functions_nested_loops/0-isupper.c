3143d9943f04804d3bb6fd77b98f3273064629da
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 05:58:38 +0000

    up1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,2 +1,21 @@
 #include "main.h"
 
+/**
+ * _isupper - Checks for uppercase character.
+ *
+ * @c: Input character.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
