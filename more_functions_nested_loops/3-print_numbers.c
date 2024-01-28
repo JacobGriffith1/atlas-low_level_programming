e695bc55b3d79930d84a30d5ca4aa39504774d7d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 06:12:18 +0000

    print1

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,2 +1,19 @@
 #include "main.h"
 
+/**
+ * print_numbers - Prints numbers 0-9 and a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
