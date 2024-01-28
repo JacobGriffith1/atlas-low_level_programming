764a968b461c6139811a053361468dbe9ce7938f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 06:49:04 +0000

    more1

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,2 +1,21 @@
 #include "main.h"
 
+/**
+ * more_numbers - Prints numbers 0-14 ten times, followed by \n.
+ *
+ * Return: N/A
+ */
+
+void more_numbers(void)
+{
+	int i, l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
