ba1e1f95e0d140160ac01ca222dcb12ac239259f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 06:33:14 +0000

    most1

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,2 +1,22 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - Prints all digits, except for 2 and 4; followed ny \n
+ *
+ * Return: N/A
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+		if (i == 49)
+			i++;
+		else if (i == 51)
+			i++;
+	}
+	_putchar('\n');
+}
