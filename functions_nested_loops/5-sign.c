795adbb9b69147f3b683efb2a6b15826488a01eb
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-28 02:00:32 +0000

    sign1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints sign of number
+ *
+ * @n: Number in ASCII
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
