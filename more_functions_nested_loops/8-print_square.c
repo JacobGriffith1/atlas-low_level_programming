bbcc69ee9b7ace61254757c55f89e1f95e0de31d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-29 01:50:34 +0000

    sq1

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,2 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * print_square - Prints a square with dimensions of size*size
+ *
+ *@size: Length and Width of square. Only used to check length.
+ *
+ * Return: N/A
+ */
+
+void print_square(int size);
+{
+	int l, h;
+
+	l = 0;
+	h = 0;
+
+	while (l < size)
+	{
+		while (h < l)
+		{
+			_putchar(10);
+			h++;
+		}
+		_putchar(35);
+	}
+	if (l == size)
+	{
+		_putchar(10);
+	}
+}
