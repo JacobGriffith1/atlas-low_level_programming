5b353afcbe3db761be54a56babd4328cde183c47
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-29 04:35:14 +0000

    sq5

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,14 +4,14 @@
 /**
  * print_square - Prints a square with dimensions of size*size
  *
- *@size: Length and Width of square. Only used to check length.
+ *@size: Height and length of square.
  *
  * Return: N/A
  */
 
 void print_square(int size)
 {
-	int l, h;
+	int h, l;
 
 	for (h = 0; h < size; h++)
 	{
