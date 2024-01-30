103ffdff23cf09ec44dcb90e516d8f3e49a21785
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 04:34:25 +0000

    array1

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,2 +1,16 @@
 #include "main.h"
 
+/**
+ * print_array - Prints n elements of an array of integers, followed by \n.
+ *
+ * @a: Array input.
+ * @n: Number of elements to print.
+ *
+ * Return: N/A
+ */
+
+void print_array(int *a, int n)
+{
+	_printf(a, n);
+	_putchar(10);
+}
