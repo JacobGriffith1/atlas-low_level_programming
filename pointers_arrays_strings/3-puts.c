bc1a9f10929c244fb07052e75d91e9ab99423434
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 02:12:22 +0000

    put1

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,2 +1,20 @@
 #include "main.h"
 
+/**
+ * _puts - Prints a string, followed by a newline.
+ *
+ * @str: String input.
+ *
+ * Return: N/A
+ */
+
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*(s + l) != '\0')
+	{
+		_putchar(*str[l]);
+		l++;
+	}
+}
