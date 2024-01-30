3aff5700e2121f9d812432870473cecf10d1bea9
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 01:37:01 +0000

    length1

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+/**
+ * _strlen - Returns length of a string.
+ *
+ * @s: Input string; will be pointed at.
+ *
+ * Return: N/A
+ */
+
+int _strlen(char *s)
+{
+	_strlen(*s);
+}
