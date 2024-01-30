7a7b4352a7420b40bb39c4d7bfddf21ce4ba20fd
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 03:47:34 +0000

    putsii1

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,2 +1,21 @@
 #include "main.h"
 
+/**
+ * _puts2 - Prints ever other character in a string, followed by a newline.
+ *
+ * @str: String input.
+ *
+ * Return: N/A
+ */
+
+void _puts2(char *str)
+{
+	int l = 0;
+
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l += 2;
+	}
+	_putchar(10);
+}
