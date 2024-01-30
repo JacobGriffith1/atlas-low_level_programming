802c4e2f00546c786e1d96bffd03e4948df35832
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 02:51:34 +0000

    rev1

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,2 +1,28 @@
 #include "main.h"
 
+/**
+ * print_rev
+ *
+ * @s: String input.
+ *
+ * Return: N/A
+ */
+
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (l >= 0)
+	{
+		if (l == '\0')
+		{
+			break;
+		}
+		l++;
+	}
+	for (l--; l >= 0, l--)
+	{
+		_puchar(s[l]);
+	}
+	_putchar(10);
+}
