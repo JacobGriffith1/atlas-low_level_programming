19df46269f3ec32b0fd754fc5093ecec4e411f6d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 01:29:52 +0000

    swap1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,2 +1,17 @@
 #include "main.h"
 
+/**
+ * swap_int - Swaps values of integers.
+ *
+ * @a: Integer A
+ * @b: Integer B
+ *
+ * Return: N/A
+ */
+
+void swap_int(int *a, int *b);
+{
+	int c = *a;
+	*a = *b;
+	*b = c
+}
