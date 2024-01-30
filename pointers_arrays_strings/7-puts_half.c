1a17639be635f9669bced019bb7dbfe4295d2041
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 04:21:31 +0000

    half1

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,2 +1,38 @@
 #include "main.h"
 
+/**
+ * puts_half - Prints half of a strijng, followed by a newline.
+ *
+ * @str: String input.
+ *
+ * Return: N/A
+ */
+
+void puts_half(char *str)
+{
+	int l = 0, h;
+
+	while (l >= 0)
+	{
+		if (str[l] == '\0')
+		{
+			break;
+		}
+		l++;
+	}
+
+	if (l % 2 == 1)
+	{
+		h = l / 2;
+	}
+	else
+	{
+		h = (l + 1) / 2;
+	}
+
+	for (h++; h < count; h++)
+	{
+		_putchar(str[h]);
+	}
+	_putchar(10);
+}
