729470aaa2fe73dc919380ff41c5da46bb5f0a62
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 01:21:52 +0000

    reset1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - updates an int value through usage of a pointer.
+ *
+ * @n: Input integer
+ *
+ * Return: N/A
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
