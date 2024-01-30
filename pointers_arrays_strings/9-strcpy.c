f08835bc3a0cf4be29eaf51132f04eb1486bbe49
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-01-30 05:05:45 +0000

    strcpy1

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,2 +1,26 @@
 #include "main.h"
 
+/**
+ * _strcpy - Copies a string to destination.
+ *
+ * @dest: Destination to print to.
+ * @src: Source. Will be copied.
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+
+	while (l >= 0)
+	{
+		*(dest + l) = *(src + l);
+		if (*(src + l) == '\0')
+		{
+			break;
+		}
+		l++;
+	}
+	return (dest);
+}
