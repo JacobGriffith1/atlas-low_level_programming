719a663064b42c0e0d63409c3b96955753176a1d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-04 05:42:58 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,32 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strncat - Concatenates two strings, but with a limit
+ *
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
+ * @n: Number of bytes used from src
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j < n)
+	{
+		*(dest + i) = (*dest + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
+}
