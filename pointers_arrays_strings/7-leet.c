513313b84a3a10c627bccbfb34f9be6bc07a24f9
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-04 09:10:21 +0000

    leet1

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,3 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * leet - encodes a string into "leetspeak"
+ * @s: Input string
+ * Return: s, but 1337
+ */
+
+char *leet(char *s)
+{
+	int i = 0, j;
+	int low[] = {97, 101, 111, 116, 108};
+	int upp[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (*(s + i) == low[j] || *(s + i) == upp[j])
+			{
+				*(s + i) = num[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
