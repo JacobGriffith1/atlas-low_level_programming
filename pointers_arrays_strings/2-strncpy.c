5e25af5f7859491e5772c3c7ad3c77eb11b24c13
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-04 06:07:32 +0000

    strncpy1

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strncpy - Copies a string
+ *
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of bytes to be used from source
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+		if (i >= n && src[i] == '\0')
+		{
+			dest[i] =  '\0';
+			break;
+		}
+	}
+	return (dest);
+}
