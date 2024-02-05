bf02309b57b3d0dc985a14d4db5bf9fc7c4e9281
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-05 20:47:44 +0000

    chess1

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,3 +2,23 @@
 #include <stdio.h>
 
 /**
+ * print_chessboard - Prints an ASCII chessboard
+ * @a - Input pointer
+ * Return: N/A
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar(10);
+		}
+		_putchar(a[i / 8][i - j]);
+	}
+	_putchar;
+}
