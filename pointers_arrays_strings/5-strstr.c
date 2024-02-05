1687a808270df542551704fef28596809f27431d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-05 19:40:52 +0000

    strstr1

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,3 +2,25 @@
 #include <stdio.h>
 
 /**
+ * _strstr - Locates a substring
+ * @haystack: Place to search for needle
+ * @needle: What to search for
+ * Return: Pointer to start of located substring, else NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+
+	for (; *(haystack + i) != '\0'; i++)
+	{
+		if (*(haystack + i) == *needle)
+		{
+			return (haystack + i);
+		}
+	}
+	if (*(haystack + i) == *needle)
+		return (haystack + i);
+	else
+		return ('\0');
+}
