4bec20b300b0124aee1b24294f1f2e56c5e0e68c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-05 07:25:55 +0000

    strbrk1

--- pointers_arrays_strings/4-strbrk.c
+++ pointers_arrays_strings/4-strbrk.c
@@ -2,3 +2,26 @@
 #include <stdio.h>
 
 /**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: String to be searched
+ * @accept: Bytes to accept
+ * Return: Position in s containing an accepted byte or NULL if none accepted
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+				break;
+			}
+		}
+	}
+	return ('\0');
+}
