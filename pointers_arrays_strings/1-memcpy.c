b4302fdf036148650e0b369b36d851ae01b23b8a
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-05 03:13:02 +0000

    memcpy1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,3 +2,19 @@
 #include <stdio.h>
 
 /**
+ * _memcpy - Copies memory area
+ * @dest: Pointer to destination to be copied to
+ * @src: Pointer to source string to copy (memory area)
+ * @n: Unsigned int that dictates how many bytes to copy
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(src + i) = *(dest + i);
+	return (dest);
+}
+
