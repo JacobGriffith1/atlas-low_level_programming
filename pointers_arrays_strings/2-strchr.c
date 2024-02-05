32e3400e02c0a1139add18c16dca25551ecd0308
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-05 06:29:42 +0000

    strchr3

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,26 +5,18 @@
  * _strchr - Locates a character in a string
  * @s: String to be searched
  * @c: Character to be found
- * Return: c if found, NULL if not
+ * Return: Position in string if found, NULL if not
  */
 
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p;
 
 	for (i = 0; *s < i; i++)
 	{
 		if (*(s + i) == c)
-		{
-			*p = *(s + i);
-			break;
-		}
+			return (s + 1);
 		else if (*(s + i) == '\0')
-		{
-			*p = '\0';
-			break;
-		}
+			return ('\0');
 	}
-	return (p);
 }
