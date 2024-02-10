3a46eb0f8c7105b02b120d7b4ab1089ed5350ccd
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-10 05:54:43 +0000

    sqrt1

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,7 +8,17 @@
  * Return: sqrt or -1
  */
 
-int find_sqrt
+int find_sqrt(int n, int c)
+{
+	if (i % (n / i) == 0)
+	{
+		if (i * (n / i) == n)
+			return (i);
+		else
+			return (-1);
+	}
+	return (0 + find_sqrt(n, i + 1));
+}
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
@@ -24,4 +34,5 @@ int _sqrt_recursion(int n)
 		return (0);
 	if (n == 1)
 		return (1);
-	return
+	return (find_sqrt(n, 2));
+}
