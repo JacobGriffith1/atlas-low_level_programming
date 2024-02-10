4d3f12f8f403ac75517d5bd47684fe11063c1c94
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-10 03:36:23 +0000

    pow1

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,3 +2,18 @@
 #include <stdio.h>
 
 /**
+ * _pow_recursion - Returns the value of x^y.
+ * @x: X value. Will be exponentially multiplied.
+ * @y: Y value. Exponent by which x will be multiplied.
+ * Return: x^y, or -1 if y is less than zero.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
