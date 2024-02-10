166b88ecab38ec8ead9f9bc0c695626a02e4db86
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-10 05:16:29 +0000

    sqrt_wip

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,3 +2,26 @@
 #include <stdio.h>
 
 /**
+ * find_sqrt - Returns the squre root of a number
+ * @n: number
+ * @i: iterator
+ * Return: sqrt or -1
+ */
+
+int find_sqrt
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to have its natural square root returned.
+ * Return: Natural square root of n, or -1 if there is no natural square root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return
