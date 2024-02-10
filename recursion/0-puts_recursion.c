b19e217406b5e5825a6a0ed289068d3d46ccb783
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-10 02:26:35 +0000

    puts1

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,3 +2,18 @@
 #include <stdio.h>
 
 /**
+ * _puts_recursion - Prints a string, followed by a newline.
+ * @s: String to be printed.
+ * Return: N/A
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar(10);
+}
