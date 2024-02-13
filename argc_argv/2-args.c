8f3be3194d4179085f0482e711e7f5e5089acb53
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-13 01:36:07 +0000

    args+1

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 
 /**
+ * main - Prints all arguments recieved
+ * @argc: Argument count
+ * @argv: Argument value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0, i < argc, i++)
+	{
+		printf("%s\n", argv[]);
+	}
+	return (0);
+}
