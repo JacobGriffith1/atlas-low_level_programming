8b5501743432f9d9479806822de09b8a5a988fa1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-13 01:54:04 +0000

    mul1

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * main - Multiplies two numbers
+ * @argc: Argument count
+ * @argv: Argument value
+ * Return: 0 - Success, 1 - Failure
+ */
+
+int main(int argc, *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
