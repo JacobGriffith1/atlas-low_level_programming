1ea06f1c669e74c4d50413a993755f5afe15f339
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-13 02:22:06 +0000

    add1

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
 
 /**
+ * main - Adds positive numbers
+ * @argc: Argument count
+ * @argv: Argument value
+ * Return: 0 - No argument/Success, 1 - Error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
