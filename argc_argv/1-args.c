9ccd168aefc5b071f1c5e8d04fd3d8148e6f973f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-13 01:16:06 +0000

    args1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Prints the number of arguments in an array
+ * @argc: Argument count
+ * @argv: Argument value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
