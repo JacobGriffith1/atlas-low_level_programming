e8015c9d5d2c72c041349cbc0431ab50e71566d9
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-15 23:54:48 +0000

    c6

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return ('\0');
 
-	a = malloc(char(c) * size);
+	a = malloc(sizeof(c) * size);
 
 	if (*a == '\0')
 		return ('\0');
