5c942134d8b692f967ed2db0fe40f0f6a4ed629e
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-16 02:22:42 +0000

    break

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * create_array - Makes array of chars, and initializes with a specific char.
  * @size: Size for the array.
  * @c: Character to be stored.
- * Return: Pointer to array on success, NUll on error or size = 0.
+ * Return: Pointer to array on success, NULL on error or size = 0.
  */
 
 char *create_array(unsigned int size, char c)
@@ -23,9 +23,14 @@ char *create_array(unsigned int size, char c)
 	{
 		return ('\0');
 	}
-	for (i = 0; (i < size) && (i != '\0'); i++)
+	for (i = 0; (i < size); i++)
 	{
 		ptr[i] = c;
+		if (ptr[i] == '\0')
+		{
+			return ('\0');
+			break;
+		}
 	}
 	return (ptr);
 }
