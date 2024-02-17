6247d95a28772dc04c3276aac870fdd196681e97
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-17 22:27:16 +0000

    cat2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k, l;
 
 	if (s1 == NULL)
-		s1 == "";
+		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (k = 0; k < i; k++)
 		ptr[k] = s1[k];
-	for (l = 0; l <= n && l <= NULL; k++, l++)
+	for (l = 0; l <= n && l <= j; k++, l++)
 		ptr[k] = s2[l];
 
 	return (ptr);
