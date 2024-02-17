b8da3fd40b1ed6bd319da434fc9896a5bb66ceb7
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-17 04:38:58 +0000

    is it this simple?

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,4 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
+ * free_grid - FREES a 2D GRID created by 'alloc_grid'.
+ * @grid: The GRID to be FREED.
+ * @height: The HEIGHT of the GRID.
+ * Return: N/A
+ */
+
+void free_grid(int **grid, int height)
+{
+	int h;
+
+	for (h = 0; h < height; h++)
+		free(grid[h]);
+}
