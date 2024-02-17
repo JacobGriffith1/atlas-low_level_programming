bae1b820f91331c522d791202017e717a3922cb6
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-17 23:49:56 +0000

    cal1

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,3 +3,27 @@
 #include <stdio.h>
 
 /**
+ * _calloc - ALLOCATES memory for an arry using [malloc].
+ * @nmemb: Number of ELEMENTS
+ * @size: Size of BYTES in each ELEMENT of [nmemb].
+ * Return: POINTER to MEMORY, NULL if ((nmemb||size) == 0) or [malloc] fails.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if ((nmemb||size) == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
