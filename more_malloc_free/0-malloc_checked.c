aceb1965d20a5970187f83f28f36ffafb0ba2911
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-17 21:20:40 +0000

    mlc1

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,3 +3,17 @@
 #include <stdio.h>
 
 /**
+ * malloc_checked - ALLOCATES MEMORY using [malloc].
+ * @b: Amount of BYTES to allocate.
+ * Return: POINTER to allocated memory, or 98 if there is  a failure.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
