e8531785ba7da4ad3231c2708d6795bb1a896aa4
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-18 00:39:28 +0000

    Min-Maxxing? In MY code??

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -3,3 +3,27 @@
 #include <stdio.h>
 
 /**
+ * array_range - Creates an ARRAY of INTEGERS
+ * @min: MINIMUM value.
+ * @max: MAXIMUM value.
+ * Return: POINTER to new ARRAY, NULL if min > max or [malloc] fails.
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * ((max - min) + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		ptr[k] = min;
+
+	return (ptr);
+}
