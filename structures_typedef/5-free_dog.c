a366f9f6d747cb2c88db2a1de3ce7ecf6cd554e7
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-25 04:19:18 +0000

    Incantation of The Baja Men

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,3 +3,17 @@
 #include <stdlib.h>
 
 /**
+ * free_dog - Unleash the hounds!
+ * @d: That dawg in me (struct dog)
+ * Return: I sure hope he does! (N/A)
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d.name);
+		free(d.owner);
+		free(d);
+	}
+}
