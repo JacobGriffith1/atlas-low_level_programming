318b21b2d5068f0bf53388eaa5fa68dec2b8d93b
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-25 01:39:01 +0000

    init1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,3 +3,20 @@
 #include <stdlib.h>
 
 /**
+ * init_dog - Initializes a variable of type 'struct dog'.
+ * @d: Pointer to the structure named 'dog'.
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The dog's owner.
+ * Return: N/A
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
