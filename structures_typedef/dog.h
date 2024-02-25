6c6292d09bb9184f5f38d5f82e259e66e837e9b0
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-25 01:24:54 +0000

    lo, the consequences of skipping ahead.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-typedef struct dog;
-
 /**
  * struct dog - Brief dog attributes.
  * @name: The dog's name.
@@ -20,7 +18,5 @@ struct dog {
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
