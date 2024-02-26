baa8e29ad054b5b73ebc36c74c81a102778a8b4d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-26 05:21:57 +0000

    headerfix

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * get_op_function - Selects which function to perform.
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * main - The culmination of my time; math that I'd rather have done on paper.
