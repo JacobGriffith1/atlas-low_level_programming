a6fa9c829a3d281be4a3ae089f96a8168d51d709
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-26 05:48:18 +0000

    could it be?

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	if (argv[2][1])
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	operation = get_op_func(argv[2]);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	if (operation == NULL)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 
 	a = atoi(argv[1]);
