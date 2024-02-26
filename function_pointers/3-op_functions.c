0e493c8a2e77f16f1e5700b1616b8e28faa6a2c1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-26 03:18:41 +0000

    Why I Hate Betty: The Movie

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,3 +1,61 @@
 #include "calc.h"
 
 /**
+ * op_add - Addition.
+ * @a: First number.
+ * @b: Second number.
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtraction.
+ * @a: First number.
+ * @b: Second number.
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplication.
+ * @a: First number.
+ * @b: Second number.
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division.
+ * @a: First number.
+ * @b: Second number.
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulo.
+ * @a: First number.
+ * @b: Second number.
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
