24621eba4261d9de77201a7cf1813f41a9766575
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-26 04:57:51 +0000

    hubuhbubuhbhhh

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,3 +1,28 @@
 #include "calc.h"
 
 /**
+ * get_op_function - Selects which function to perform.
+ * @s: string to check for matching code.
+ * Return: Operation to perform or NULL.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
+	return (ops[i / 2].f);
+}
