6054e471584847ece1582940a634b2d87e301b4b
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-02-27 04:47:35 +0000

    sum1

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,3 +8,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list val;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(val, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(val, int);
+
+	va_end(val);
+
+	return (sum);
+}
