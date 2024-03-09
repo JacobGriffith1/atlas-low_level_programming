61abbe07c75a03bb9a55cf096bc83c1cb21b8f13
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-09 23:49:33 +0000

    print_list first attempt

--- singly_linked_lists/main.h
+++ singly_linked_lists/main.h
@@ -1,6 +1,24 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_t - singly linked list
+ * @str: string - contents of the node
+ * @len: length - length of the string
+ * @next: points to the next node
+ */
+
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
