20277704253d94456c405ec4fefbd746eac01aa1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-10 21:12:14 +0000

    print_listint first attempt

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,20 @@
 #include "lists.h"
 
 /**
+ * print_listint - Prints all the elements of a list_t list.
+ * @h: Head of a list.
+ * Return: Number of nodes (nodeCount).
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
