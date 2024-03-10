ad7bc5d4af948515e1e93f552770edb43678f756
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-10 02:05:05 +0000

    free_list first attempt

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,3 +1,19 @@
 #include "lists.h"
 
 /**
+ * free_list - Frees a 'list_t' list.
+ * @head: First node of a linked list.
+ * Return: N/A
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while ((node = head) != NULL)
+	{
+		head = head->next;
+		free(node->str);
+		free(node);
+	}
+}
