9a585ecba6e8d3a58553263700a089a952fe512f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-10 01:42:09 +0000

    add_node first attempt

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,3 +1,29 @@
 #include "lists.h"
 
 /**
+ * add_node - Adds a new node at the beginning of a 'list_t' list.
+ * @head: First node of linked list.
+ * @str: string to add to linked list.
+ * Return: Address of new element or NULL if failed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t charCount;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (charCount = 0; str[charCount]; charCount++)
+		;
+
+	new->len = charCount;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
