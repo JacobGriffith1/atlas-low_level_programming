5e78342bb12beb08ce5779d88f96369f478017ce
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-10 21:38:32 +0000

    listint_len first attempt

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,3 +1,19 @@
 #include "lists.h"
 
 /**
+ * listint_len - Returns the number of elements in a linked list_t list.
+ * @h: Head of the linked list.
+ * Return: Number of elements in the linked list (nodeCount)
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
