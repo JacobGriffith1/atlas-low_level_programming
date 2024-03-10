0b729763fd7afe2a6b81ba45eaf4ee6fccb62d0c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-10 00:35:35 +0000

    list_len first attempt

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,3 +1,19 @@
 #include "lists.h"
 
 /**
+ * list_len - Returns the number of elements in a linked 'list_t' list.
+ * @h: Singly linked list.
+ * Return: Number of elements; elemCount
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elemCount = 0;
+
+	while (h->str != NULL)
+	{
+		h = h->next;
+		elemCount++;
+	}
+	return (elemCount);
+}
