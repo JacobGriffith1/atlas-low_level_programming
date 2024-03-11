2a158aabf25a660420cd490515b267cf33d17e6e
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-11 02:26:26 +0000

    comparison fix in first if statement

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	node = *head;
 
-	if (idx == 0)
+	if (idx != 0)
 	{
 		for (i = 0; i < idx && node != NULL; i++)
 			node = node->next;
