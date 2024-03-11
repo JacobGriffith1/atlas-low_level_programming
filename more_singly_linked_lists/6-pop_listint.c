72066c4a7ac20a9cb97bcf5a27fe40207933d745
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-11 01:11:36 +0000

    pop_listint first attempt

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,3 +1,26 @@
 #include "lists.h"
 
 /**
+ * pop_listint - Deletes the head node of a listint_t list,
+ * and returns the head node's data (n).
+ * @head: Head of the list.
+ * Return: Head node's data (n), or 0 if list is empty.
+ */
+
+int pop_listint(listint_t **head)
+{
+	int nodeData;
+	listint_t *node, *new;
+
+	if (*head == NULL)
+		return (0);
+
+	node = *head;
+	nodeData = node->n;
+	new = node->next;
+
+	free(node);
+	*head = new;
+
+	return (nodeData);
+}
