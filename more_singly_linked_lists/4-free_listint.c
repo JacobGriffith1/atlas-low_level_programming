5a9b545323279bfe50dc55758f3bff64f4e1bab3
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-11 00:28:08 +0000

    free_listint first attempt

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,3 +1,19 @@
 #include "lists.h"
 
 /**
+ * free_listint - Frees a listint_t list.
+ * @head: Head of a list. Will be reassigned to point at each node
+ * during the freeing process.
+ * Return: N/A
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while ((node = head) != NULL)
+	{
+		head = head->next;
+		free(node);
+	}
+}
