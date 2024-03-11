7eef91c5ef9db07cb1c22396468bc3753ea5eff9
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-11 00:41:31 +0000

    free_listint2 first attempt

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,3 +1,24 @@
 #include "lists.h"
 
 /**
+ * free_listint2 - Frees a listint_t list, sets head to NULL.
+ * @head: Head of a string. Will be changed during the freeing process
+ * to each other node, and then NULL after freeing is complete.
+ * Return: N/A
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *temp;
+
+	if (head != NULL)
+	{
+		node = *head;
+		while ((temp = node) != NULL)
+		{
+			node = node->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
