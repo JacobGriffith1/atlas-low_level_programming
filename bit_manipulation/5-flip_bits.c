f57834291a52d9b29962de620b890642bfe80101
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-12 03:03:08 +0000

    flip_bits attempt #1

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,3 +1,21 @@
 #include "main.h"
 
 /**
+ * flip_bits - Returns the number of bits to flip to get from
+ * one number to another.
+ * @n: First number input.
+ * @m: Second number input.
+ * Return: Number of bits required to flip (bitCount).
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bitCount;
+
+	for (bitCount = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			bitCount++;
+	}
+	return (bitCount);
+}
