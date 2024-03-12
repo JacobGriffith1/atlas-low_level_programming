058fe41eaea5b9ad2ca0459cef3539ee159428c8
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-12 04:40:13 +0000

    append_text_to_file attempt (1)

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,3 +1,35 @@
 #include "main.h"
 
 /**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file.
+ * @text_content: String to append to the file.
+ * Return: 1 (SUCCESS), -1 (FAILURE)
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int f, letCount, rwr;
+
+	if (!filename)
+		return (-1);
+
+	f = open(filename, O_WRONLY | O_APPEND);
+
+	if (f == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (letCount = 0; text_content[letCount]; letCount++)
+			;
+
+		rwr = write(f, text_content, letCount);
+
+		if (rwr == -1)
+			return (-1);
+	}
+	close(f);
+
+	return (1);
+}
