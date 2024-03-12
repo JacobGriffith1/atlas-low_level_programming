ac9bbd627465e73113931a2a28cd03dbbdcd01f2
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-12 02:11:11 +0000

    get_bit attempt #1

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,3 +1,23 @@
 #include "main.h"
 
 /**
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: Input.
+ * @index: The index, starting from 0.
+ * Return: Value of the bit at index or -1 if error.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+			return (n & 1);
+	}
+	return (-1);
+}
