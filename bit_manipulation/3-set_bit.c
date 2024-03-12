0aa92eee742ce3d6b2f462d508bcc6d13a0e43e7
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-12 02:32:48 +0000

    set_bit attempt #1

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,3 +1,21 @@
 #include "main.h"
 
 /**
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: Input.
+ * @index: Index, starting from 0 of the bit to set.
+ * Return: 1 (SUCCESS), -1(ERROR)
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 63)
+		return (-1);
+
+	i = 1 << index;
+	*n = (*n | i);
+
+	return (1);
+}
