2bea2944e90a6d35363befd7e4c6251e0e7472cb
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-12 02:56:46 +0000

    clear_bit attempt #1

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,3 +1,23 @@
 #include "main.h"
 
 /**
+ * clear_bit - Sets the value of a bit to 0 at a given index.
+ * @n: Input
+ * @index: Index, starting from 0 of the bit to set.
+ * Return: 1 (SUCCESS), -1 (ERROR).
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 63)
+		return (-1);
+
+	i = 1 << index;
+
+	if (*n & i)
+		*n ^= i;
+
+	return (1);
+}
