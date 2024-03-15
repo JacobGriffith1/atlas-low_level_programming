f7508106a0cfafc1b49119be8e28d7b5d4372520
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-15 02:22:28 +0000

    dlistint_len attempt (1)

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,3 +1,23 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list.
+ * @h: Head of the list.
+ * Return: Number of nodes in list (nodeCt).
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int nodeCt = 0;
+
+	if (h == NULL)
+		return (nodeCt);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		nodeCt++;
+		h = h->next;
+	}
+	return (nodeCt);
+}
