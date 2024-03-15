07db3dfe87e4a40f6bed3802dcc1c6087421f9e4
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-15 02:57:35 +0000

    add_dnodeint_end attempt (2); Adjusted to handle adding onto an empty list

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,9 +23,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		while (h->next != NULL)
 			h = h->next;
-	}
-	new->prev = h;
-	if (h != NULL)
 		h->next = new;
+	}
+	else
+		*head = new;
+
+	new->prev = h;
 	return (new);
 }
