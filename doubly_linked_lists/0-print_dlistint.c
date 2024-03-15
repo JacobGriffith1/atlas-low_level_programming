cca44029372d387ed53076a2848c3fe03b05af13
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-15 02:10:55 +0000

    print_dlistint attempt (1)

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,3 +1,24 @@
 #include "lists.h"
 
 /**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Head of the list.
+ * Return: Number of nodes in list.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int nodeCt = 0;
+
+	if (h == NULL)
+		return (nodeCt);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodeCt++;
+		h = h->next;
+	}
+	return (nodeCt);
+}
