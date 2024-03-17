55a15b4bf30c033dc4861376278c5bc5935a5632
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-17 23:29:24 +0000

    hash_table_print attempt (1)

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,3 +1,32 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table.
+ * Return: N/A
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	char *x;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	x = "";
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			printf("%s'%s': '%s'", x, tmp->key, tmp->value);
+			x = ", ";
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
