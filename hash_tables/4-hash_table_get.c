9f5ac8eef54cd4641e583828eeb46ddc0b106595
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-17 23:12:19 +0000

    hash_table_get attempt (1)

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,3 +1,32 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table to look into.
+ * @key: Key to search for.
+ * Return: Value, or NULL if no key is found.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int kIndex;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+		return (NULL);
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	kIndex = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[kIndex];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
