b124843ac9b71804e482eb92f0a5b3dba3870e46
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-17 22:06:16 +0000

    key_index attempt (1)

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,3 +1,16 @@
 #include "hash_tables.h"
 
 /**
+ * key_index - Gives you the key of an index.
+ * @key: The key.
+ * @size: The size of the array of the hash table.
+ * Return: Index at which the key/value pair should be
+ * stored in the array of the hash table.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (key == NULL || size == 0)
+		return (0);
+	return (hash_djb2(key) % size);
+}
