38271ee04a86e96bd72dbe6e7c84d584f3a88876
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-29 23:39:59 +0000

    isostandard

--- atlas_ss/type_prompt.c
+++ atlas_ss/type_prompt.c
@@ -15,7 +15,7 @@ void type_prompt(void)
 
 	if (first_time) /* clear screen for the first time */
 	{
-		const char *CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
+		const char *CLEAR_SCREEN_ANSI = " \033[1;1H\033[2J";
 
 		write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
 		first_time = 0;
