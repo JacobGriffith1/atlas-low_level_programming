75a39e99a0668127e37d4ce83cfdc8275ba3e64d
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-30 01:59:35 +0000

    Learningtoshell4

--- atlas_ss/read_command.c
+++ atlas_ss/read_command.c
@@ -4,6 +4,13 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+ * read_command - Reads and parses contents of user input.
+ * @cmd: Commands.
+ * @par: Parameters.
+ * Return: N/A
+ */
+
 void read_command(char cmd[], char *par[])
 {
 	char line[1024];
@@ -13,6 +20,7 @@ void read_command(char cmd[], char *par[])
 	for (;;)	/* Read one line */
 	{
 		int c = fgetc(stdin);
+
 		line[count++] = (char) c;
 
 		if (c == '\n')
@@ -28,9 +36,9 @@ void read_command(char cmd[], char *par[])
 		pch = strtok(NULL, " \n");
 	}
 
-	strcpy (cmd, array[0]);
+	strcpy(cmd, array[0]);
 
-	for (int j = 0; j < i; j++)
+	for (; j < i; j++)
 		par[j] = array[j];
 	par[i] = NULL;	/* NULL-terminate the parameter list */
 }
