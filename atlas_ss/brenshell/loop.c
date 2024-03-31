bd5350f752e1d4042df4afc999976dcf26730fe8
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-03-31 20:34:12 +0000

    Learningtoshell_6

--- atlas_ss/brenshell/loop.c
+++ atlas_ss/brenshell/loop.c
@@ -1,5 +1,24 @@
 #include "shell.h"
 
 /**
- * lsh_loop
+ * lsh_loop - Loop through the shell.
  */
+
+void lsh_loop(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do
+	{
+		printf("> ");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	}
+	while (status);
+}
---
+++ atlas_ss/brenshell/read_line.c
@@ -0,0 +1,8 @@
+#include "shell.h"
+#define LSH_RL_BUFSIZE 1024
+
+/**
+ * lsh_read_line - Reads the user input.
+ */
+
+
