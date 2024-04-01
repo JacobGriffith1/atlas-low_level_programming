cff6e4a89d4587cf95096c0ef47fd2543390b3ed
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-01 06:36:13 +0000

    call4danight

--- atlas_ss/brenshell/shell.h
+++ atlas_ss/brenshell/shell.h
@@ -40,6 +40,7 @@ void lsh_loop(void);
 char *lsh_read_line(void);
 char **lsh_split_line(char *line);
 int lsh_launch(char **args);
+int lsh_num_builtins(void);
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
