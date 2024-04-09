9a96f8e8fe608d5a9d0277a96e6ac59afe2a09a0
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-09 01:52:56 +0000

    deleted an old directory that I no longer need. It didn`t do too much anyway :/

--- atlas_ss/shell.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-
-int main(void);
-void read_command(char cmd[], char *par[]);
-void type_prompt(void);
-int ss_exit(void);
-
-#endif
--- atlas_ss/ss_exit.c
+++
@@ -1,7 +0,0 @@
-#include "shell.h"
-
-int ss_exit(void)
-{
-	if (1)
-		break;
-}
