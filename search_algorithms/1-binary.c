1f0c2cd30cc86f1289df79f3a8c6fe68a126b06f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-13 03:50:57 +0000

    callin it for 2nite

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -18,7 +18,10 @@ int binary_search(int *array, size_t size, int value)
 
 	while (l <= r)
 	{
+		printf("Searching in array");
+
 		m = ((l + r) / 2);
+
 		if (array[m] < value)
 			l = (m + 1);
 		else if (array[m] > value)
